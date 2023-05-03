19d85093b72b7c6cd77906ec5e9852c182c57709
0921ryang <0921ryang@gmail.com>
2023-05-03 16:15:26 +0200

    c

--- src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
+++ src/test/java/de/tum/in/ase/eist/PersonIntegrationTest.java
@@ -17,8 +17,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -110,7 +109,7 @@ class PersonIntegrationTest {
         JSONObject responseJson1 = new JSONObject(responseBody1);
         Long id1 = responseJson1.getLong("id");
         var response2 = this.mvc.perform(
-                put("/persons/"+id+"/parents")
+                put("/persons/" + id + "/parents")
                         .content(objectMapper.writeValueAsString(parent))
                         .contentType("application/json")
         ).andReturn().getResponse();
@@ -133,7 +132,126 @@ class PersonIntegrationTest {
     }
 
     @Test
-    void testAddThreeParents() {
+    void testAddThreeParents() throws Exception{
+        var child = new Person();
+        var parent1 = new Person();
+        var parent2 = new Person();
+        var parent3 = new Person();
+        LocalDate localDate = LocalDate.now();
+        child.setFirstName("I");
+        child.setLastName("J");
+        child.setBirthday(LocalDate.MIN);
+        parent1.setFirstName("K");
+        parent1.setLastName("L");
+        parent1.setBirthday(localDate);
+        parent2.setFirstName("E");
+        parent2.setLastName("F");
+        parent2.setBirthday(localDate);
+        parent3.setFirstName("G");
+        parent3.setLastName("H");
+        parent3.setBirthday(localDate);
 
+        var response = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(child))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        var response1 = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent1))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        var response2 = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent2))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        var response3 = this.mvc.perform(
+                post("/persons")
+                        .content(objectMapper.writeValueAsString(parent3))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertEquals(HttpStatus.OK.value(), response1.getStatus());
+        assertEquals(HttpStatus.OK.value(), response2.getStatus());
+        assertEquals(HttpStatus.OK.value(), response3.getStatus());
+
+        assertEquals(4, personRepository.findAll().size());
+
+        String responseBody = response.getContentAsString();
+        JSONObject responseJson = new JSONObject(responseBody);
+        Long id = responseJson.getLong("id");
+
+        String responseBody1 = response1.getContentAsString();
+        JSONObject responseJson1 = new JSONObject(responseBody1);
+        Long id1 = responseJson1.getLong("id");
+
+        String responseBody2 = response2.getContentAsString();
+        JSONObject responseJson2 = new JSONObject(responseBody2);
+        Long id2 = responseJson2.getLong("id");
+
+        String responseBody3 = response3.getContentAsString();
+        JSONObject responseJson3 = new JSONObject(responseBody3);
+        Long id3 = responseJson3.getLong("id");
+
+        var responsePut = this.mvc.perform(
+                put("/persons/" + id + "/parents")
+                        .content(objectMapper.writeValueAsString(parent1))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), responsePut.getStatus());
+        assertEquals(4, personRepository.findAll().size());
+
+        assertTrue(personRepository.existsById(id));
+        assertTrue(personRepository.existsById(id1));
+        assertTrue(personRepository.existsById(id2));
+        assertTrue(personRepository.existsById(id3));
+
+        String responseBody0 = responsePut.getContentAsString();
+        JSONObject responseJson0 = new JSONObject(responseBody0);
+        JSONArray parentsJson = responseJson0.getJSONArray("parents");
+        JSONObject parentJson = parentsJson.getJSONObject(0);
+        String firstName = parentJson.getString("firstName");
+        String lastName = parentJson.getString("lastName");
+        LocalDate birthday = LocalDate.parse(parentJson.getString("birthday"));
+        assertEquals("K", firstName);
+        assertEquals("L", lastName);
+        assertEquals(localDate, birthday);
+
+        var responsePut1 = this.mvc.perform(
+                put("/persons/" + id + "/parents")
+                        .content(objectMapper.writeValueAsString(parent2))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+
+        assertEquals(HttpStatus.OK.value(), responsePut1.getStatus());
+        assertEquals(4, personRepository.findAll().size());
+        assertTrue(personRepository.existsById(id));
+        assertTrue(personRepository.existsById(id1));
+        assertTrue(personRepository.existsById(id2));
+        assertTrue(personRepository.existsById(id3));
+
+        String responseBody00 = responsePut1.getContentAsString();
+        JSONObject responseJson00 = new JSONObject(responseBody00);
+        JSONArray parentsJson0 = responseJson00.getJSONArray("parents");
+        JSONObject parentJson0 = parentsJson0.getJSONObject(0);
+        String firstName0 = parentJson0.getString("firstName");
+        String lastName0 = parentJson0.getString("lastName");
+        LocalDate birthday0 = LocalDate.parse(parentJson0.getString("birthday"));
+        assertEquals("E", firstName0);
+        assertEquals("F", lastName0);
+        assertEquals(localDate, birthday0);
+
+        var responseFalse = this.mvc.perform(
+                put("/persons/" + id + "/parents")
+                        .content(objectMapper.writeValueAsString(parent3))
+                        .contentType("application/json")
+        ).andReturn().getResponse();
+        assertEquals(400, responseFalse.getStatus());
     }
 }
